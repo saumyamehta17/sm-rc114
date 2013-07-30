1005471f675255492ad5b705c165ec6b4d7f620d
sweety <smehta1289@gmail.com>
2013-07-31 00:44:14 +0530

    enless scrooling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc114::Application.config.secret_token = 'd128086c6013c182476ddcc4441cc31ec0d89b655a2d9cf813d0e9e80603e4394805dfc9f1da285636f96cc93fb6bee75fd70a2fc9df22f63f05e3d68c2279fa'
